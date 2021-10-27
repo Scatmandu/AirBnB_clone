a976dc15c506a30f3552285d94a3d3f58e33c79c
Esoteric918 <3411@holbertonschool.com>
2021-10-27 15:33:14 -0500

    update for task 8 and 9

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Model defines class User'''
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    '''Defining Review inherited from BaseModel'''
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
