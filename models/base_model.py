763ac3c9af88f7276662d90a72e6b219944f6099
Esoteric918 <3411@holbertonschool.com>
2021-10-29 16:13:50 -0500

    add test files

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         '''Method to return a string representation'''
